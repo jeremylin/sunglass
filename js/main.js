1e522c506e45a64f7f1061d8d655289eafb6aa0b
SDLyu <dragon90321@gmail.com>
2017-01-14 13:52:28 +0800

    Update products and refectoring code

--- js/main.js
+++ js/main.js
@@ -53,7 +53,6 @@ $(function () {
     selectedItems = selectedItems.reduce(function (previous, after) {
       var inItem = false
       for (key in previous) {
-
         if (previous[key].product_id == after.product_id) {
           previous[key].quantity += after.quantity
           inItem = true
